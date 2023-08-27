69fe77a57e4c2ddb1a0ce3b16e1d0fdb1e531b3a
Gabriel <102957524+gabri7sc@users.noreply.github.com>
2023-08-27 16:24:48 -0300

    Update rossmann-bot.py

--- rossmann-bot.py
+++ rossmann-bot.py
@@ -57,7 +57,7 @@ def load_dataset( store_id ):
 def predict( data ):
 
     # API call
-    url = 'https://telegram-bot-yfr8.onrender.com/rossmann/predict'
+    url = 'https://teste-rossmann-prediction-api.onrender.com/rossmann/predict'
 
     header = {'Content-type': 'application/json' }
     data = data
